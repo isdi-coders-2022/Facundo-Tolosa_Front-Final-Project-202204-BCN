8329ba0077f03302f1bb7cdddededea321d6ec0b
Facundo Tolosa <facundotolosa98@gmail.com>
2022-05-31 21:21:16 +0200

    Fifth attempt :(

--- src/redux/thunks/userThunks/userThunks.test.ts
+++ src/redux/thunks/userThunks/userThunks.test.ts
@@ -28,7 +28,7 @@ describe("Given a loginThunk", () => {
       const thunk = loginThunk({ username: carlosInfo.username, password: "" });
       const action = loginActionCreator(carlosInfo);
 
-      Object.defineProperty(window, "localStorage", {
+      Object.defineProperty(Window, "localStorage", {
         value: fakeLocalStorage,
       });
 
