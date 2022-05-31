e6f92d2cd35fcf219a6681b55c2aaf74b4272f2e
Facundo Tolosa <facundotolosa98@gmail.com>
2022-05-31 20:48:23 +0200

    Second try to mock localstorage

--- src/redux/thunks/userThunks/userThunks.test.ts
+++ src/redux/thunks/userThunks/userThunks.test.ts
@@ -9,12 +9,23 @@ jest.mock("jwt-decode", () => () => ({
   image: "carlos.jpg",
 }));
 
+const fakeLocalStorage = (function () {
+  let store = {};
+  return {
+    setItem: jest.fn(),
+  };
+})();
+
 describe("Given a loginThunk", () => {
   describe("When invoked with a valid user", () => {
     test("Then it should call the dispatch with a login action creator and the info of the valid user", async () => {
       const dispatch = jest.fn();
       const thunk = loginThunk({ username: carlosInfo.username, password: "" });
       const action = loginActionCreator(carlosInfo);
+
+      Object.defineProperty(window, "localStorage", {
+        value: fakeLocalStorage,
+      });
 
       await thunk(dispatch);
 
