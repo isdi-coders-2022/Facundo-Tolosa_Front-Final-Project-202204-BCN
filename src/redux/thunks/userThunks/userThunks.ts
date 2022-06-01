524a7a0172e3a3b065bdf24248fd6a563342d51d
Facundo Tolosa <facundotolosa98@gmail.com>
2022-06-01 12:11:09 +0200

    Login thunk created

---
+++ src/redux/thunks/userThunks/userThunks.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+import jwtDecode from "jwt-decode";
+import { loginActionCreator } from "../../features/userSlice/userSlice";
+import { AppDispatch } from "../../store/store";
+
+interface IuserCredentials {
+  username: String;
+  password: String;
+}
+
+interface LoginResponse {
+  token: string;
+}
+
+interface IuserInfo {
+  id: String;
+  name: String;
+  username: String;
+  image: String;
+}
+
+export const loginThunk =
+  (userData: IuserCredentials) => async (dispatch: AppDispatch) => {
+    const { data } = await axios.post<LoginResponse>(
+      `${process.env.REACT_APP_API_URL}user/login`,
+      userData
+    );
+
+    window.localStorage.setItem("token", data.token);
+
+    const { id, name, username, image }: IuserInfo = jwtDecode<IuserInfo>(
+      data.token
+    );
+
+    dispatch(loginActionCreator({ id, name, username, image }));
+  };
