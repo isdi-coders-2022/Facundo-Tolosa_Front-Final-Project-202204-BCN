0c428fb7c9fad55dfb82eeee0f2befdd1c9c67c9
Facundo Tolosa <facundotolosa98@gmail.com>
2022-06-01 10:01:05 +0200

    Added test for registerform

---
+++ src/Components/RegisterForm/RegisterForm.test.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import RegisterForm from "./RegisterForm";
+import userEvent from "@testing-library/user-event";
+import { BrowserRouter } from "react-router-dom";
+
+describe("Given a RegisterForm component", () => {
+  describe("When the word 'hello' is written to the name input field", () => {
+    test("Then the value of the username input field should be 'hello'", () => {
+      const labelToFind = "Username";
+      const inputText = "hello";
+
+      render(
+        <BrowserRouter>
+          <RegisterForm />
+        </BrowserRouter>
+      );
+      const label: HTMLLabelElement = screen.getByLabelText(labelToFind);
+      userEvent.type(label, inputText);
+
+      expect(label).toHaveValue(inputText);
+    });
+  });
+
+  describe("When the two inputs have text and the submit button is clicked", () => {
+    test("Then the two inputs should be empty", () => {
+      const usernameLabel = "Username";
+      const passwordLabel = "Password";
+      const inputText = "hello";
+
+      render(<RegisterForm />);
+
+      const username: HTMLLabelElement = screen.getByLabelText(usernameLabel);
+      const password: HTMLLabelElement = screen.getByLabelText(passwordLabel);
+      const submitButton: HTMLButtonElement = screen.getByRole("button");
+
+      userEvent.type(username, inputText);
+      userEvent.type(password, inputText);
+      userEvent.click(submitButton);
+
+      expect(username).toHaveValue("");
+      expect(password).toHaveValue("");
+    });
+  });
+});
