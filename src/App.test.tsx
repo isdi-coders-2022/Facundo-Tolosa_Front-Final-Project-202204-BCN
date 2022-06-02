7407a33d34aa5de9cfd4368da91421ddc4f50b70
Facundo Tolosa <facundotolosa98@gmail.com>
2022-06-02 23:43:07 +0200

    App component tested

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,10 +4,48 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import store from "./redux/store/store";
 import { Provider } from "react-redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+jest.mock("jwt-decode", () => () => ({
+  id: "1",
+  name: "carlos",
+  username: "carlos",
+  image: "carlos.jpg",
+}));
+
+const mockUseNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockUseNavigate,
+}));
 
 describe("Given an App component", () => {
-  describe("When it's rendered", () => {
-    test("Then it should show the text 'AMAZING NOTES'", () => {
+  describe("When it's rendered and there is an user logged", () => {
+    test("Then it should call navigate with the route '/home'", () => {
+      const userMockSlice = createSlice({
+        name: "user",
+        initialState: { name: "Carlos" },
+        reducers: {},
+      });
+      const mockStore = configureStore({
+        reducer: { user: userMockSlice.reducer },
+      });
+
+      render(
+        <BrowserRouter>
+          <Provider store={mockStore}>
+            <App />
+          </Provider>
+        </BrowserRouter>
+      );
+
+      expect(mockUseNavigate).toHaveBeenCalledWith("/home");
+    });
+  });
+
+  describe("When it's rendered and there is no user logged", () => {
+    test("Then it should render the text 'AMAZING NOTES'", () => {
       const expectedText = "AMAZING NOTES";
 
       render(
