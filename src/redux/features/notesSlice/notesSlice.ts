78d9fa4280620971cb9a2ad92424849c4d66646d
Facundo Tolosa <facundotolosa98@gmail.com>
2022-06-03 23:41:30 +0200

    Delete note action

--- src/redux/features/notesSlice/notesSlice.ts
+++ src/redux/features/notesSlice/notesSlice.ts
@@ -21,6 +21,10 @@ const notesSlice = createSlice({
       ...user,
       allNotes: [...action.payload],
     }),
+    deleteNote: (user, action: PayloadAction<string>): IinitialState => ({
+      ...user,
+      allNotes: user.allNotes.filter((note) => note.id !== action.payload),
+    }),
   },
 });
 
