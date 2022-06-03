85a571dd7eb06b4c47476d32c29927f99ac16ffa
Facundo Tolosa <facundotolosa98@gmail.com>
2022-06-04 01:01:50 +0200

    User card component created and tested

---
+++ src/Components/UserCard/UserCardStyles.ts
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+const UserCardContainer = styled.div`
+  width: 300px;
+  height: 270px;
+  background-color: #8594e4;
+  border-radius: 15px;
+  transition: all 0.5s;
+  font-family: "Work Sans", sans-serif;
+  box-shadow: 5px 5px 9px -4px rgba(0, 0, 0, 0.58);
+  margin: 10px;
+  display: inline-flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+
+  p {
+    color: #fff;
+    margin: 0px;
+  }
+
+  .user-card__name {
+    font-size: 30px;
+    font-weight: bold;
+    margin: 5px 0;
+  }
+
+  .username-name-img {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  img {
+    width: 150px;
+  }
+
+  @media (min-width: 600px) {
+    :hover {
+      box-shadow: 5px 5px 9px -2px rgba(0, 0, 0, 0.58);
+    }
+  }
+`;
+
+export default UserCardContainer;
