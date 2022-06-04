99a4e5949a1638e86d9b5f9d3874c242e89da28c
Facundo Tolosa <facundotolosa98@gmail.com>
2022-06-04 18:37:45 +0200

    Tests fixed

--- src/redux/thunks/notesThunks/notesThunks.test.ts
+++ src/redux/thunks/notesThunks/notesThunks.test.ts
@@ -1,9 +1,14 @@
 import axios from "axios";
-import { deleteNoteThunk, loadNotesThunk } from "./notesThunks";
+import {
+  deleteNoteThunk,
+  getUserNotesThunk,
+  loadNotesThunk,
+} from "./notesThunks";
 import { notesMock } from "../../../mocks/notesMocks";
 import {
   deleteNoteActionCreator,
   loadNotesActionCreator,
+  setNotesToShowActionCreator,
 } from "../../features/notesSlice/notesSlice";
 
 describe("Given the loadNotesThunk function", () => {
@@ -21,6 +26,19 @@ describe("Given the loadNotesThunk function", () => {
       await thunk(dispatch);
 
       expect(dispatch).toHaveBeenCalledWith(action);
+    });
+  });
+
+  describe("When it's called and there is no token", () => {
+    test("Then it should not call dispatch", async () => {
+      const dispatch = jest.fn();
+
+      jest.spyOn(Storage.prototype, "getItem").mockReturnValue("");
+
+      const thunk = loadNotesThunk();
+      await thunk(dispatch);
+
+      expect(dispatch).not.toHaveBeenCalled();
     });
   });
 });
@@ -50,6 +68,36 @@ describe("Given the deleteNoteThunk function", () => {
       jest.spyOn(Storage.prototype, "getItem").mockReturnValue("");
 
       const thunk = deleteNoteThunk(idToDelete);
+      await thunk(dispatch);
+
+      expect(dispatch).not.toHaveBeenCalled();
+    });
+  });
+});
+
+describe("Given the getUserNotesThunk function", () => {
+  describe("When it's called with an user", () => {
+    test("Then it should call dispatch with the set notes to show action with the notes received from the axios request", async () => {
+      const dispatch = jest.fn();
+      const action = setNotesToShowActionCreator(notesMock);
+
+      jest.spyOn(Storage.prototype, "getItem").mockReturnValue("token");
+      axios.get = jest.fn().mockResolvedValue({ data: { notes: notesMock } });
+
+      const thunk = getUserNotesThunk("user");
+      await thunk(dispatch);
+
+      expect(dispatch).toHaveBeenCalledWith(action);
+    });
+  });
+
+  describe("When it's called and there is no token", () => {
+    test("Then it should not call dispatch", async () => {
+      const dispatch = jest.fn();
+
+      jest.spyOn(Storage.prototype, "getItem").mockReturnValue("");
+
+      const thunk = getUserNotesThunk("user");
       await thunk(dispatch);
 
       expect(dispatch).not.toHaveBeenCalled();
