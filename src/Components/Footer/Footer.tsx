5e7deb733c52cea7b4229fa4df5b62d1091ddc5b
Facundo Tolosa <facundotolosa98@gmail.com>
2022-06-07 00:39:38 +0200

    Pagination implemented and tested

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,11 +1,23 @@
+import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
+import {
+  decrementPageActionCreator,
+  incrementPageActionCreator,
+} from "../../redux/features/notesSlice/notesSlice";
 import FooterContainer from "./FooterStyles";
 
 const Footer = (): JSX.Element => {
+  const { actualPage } = useAppSelector((state) => state.notes);
+  const dispatch = useAppDispatch();
+
   return (
     <FooterContainer>
-      <button>{"<<"}</button>
-      <p>0</p>
-      <button>{">>"}</button>
+      <button onClick={() => dispatch(decrementPageActionCreator())}>
+        {"<<"}
+      </button>
+      <p>{actualPage}</p>
+      <button onClick={() => dispatch(incrementPageActionCreator())}>
+        {">>"}
+      </button>
     </FooterContainer>
   );
 };
