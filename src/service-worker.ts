814feb4deb9f7079a12b431cafe72bf0befa4272
Facundo Tolosa <facundotolosa98@gmail.com>
2022-06-11 20:14:43 +0200

    Intercepted POST, PUT and DELETE requests to make them offline

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,9 +1,10 @@
 /// <reference lib="webworker" />
 
+import { BackgroundSyncPlugin } from "workbox-background-sync";
 import { clientsClaim } from "workbox-core";
 import { precacheAndRoute } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
-import { CacheFirst, NetworkFirst } from "workbox-strategies";
+import { CacheFirst, NetworkFirst, NetworkOnly } from "workbox-strategies";
 
 declare const self: ServiceWorkerGlobalScope;
 
@@ -23,4 +24,35 @@ registerRoute(
     cacheName: "api-responses",
   }),
   "GET"
+);
+
+const bgSyncPlugin = new BackgroundSyncPlugin("myQueueName", {
+  maxRetentionTime: 24 * 60, // Retry for max of 24 Hours (specified in minutes)
+});
+
+registerRoute(
+  ({ url }) =>
+    url.origin === "https://facundo-tolosa-final-project-back.onrender.com",
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  "DELETE"
+);
+
+registerRoute(
+  ({ url }) =>
+    url.origin === "https://facundo-tolosa-final-project-back.onrender.com",
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  "PUT"
+);
+
+registerRoute(
+  ({ url }) =>
+    url.origin === "https://facundo-tolosa-final-project-back.onrender.com",
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  "POST"
 );
